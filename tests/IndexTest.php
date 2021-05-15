1faeb20207d638fdcf3d712cc0ce6884f3d9fc76
Linus Metzler <hi@linusmetzler.me>
2021-05-15 14:39:45 +0000

    more tests

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -2,6 +2,8 @@
 
 namespace Limenet\LaravelElasticaBridge\Tests;
 
+use Elastica\Client;
+use Elastica\Index;
 use Limenet\LaravelElasticaBridge\Tests\App\Elasticsearch\CustomerIndex;
 
 class IndexTest extends TestCase
@@ -15,11 +17,20 @@ class IndexTest extends TestCase
         $this->customerIndex = $this->app->make(CustomerIndex::class);
     }
     /** @test */
-    public function index_settings()
+    public function raw_index()
+    {
+        $index = $this->customerIndex->getElasticaIndex();
+        $this->assertInstanceOf(Index::class, $index);
+        $this->assertSame($this->customerIndex->getName(),$index->getName());
+    }
+
+    /** @test */
+    public function settings()
     {
         $settings = $this->customerIndex->getCreateArguments();
-        if ($this->customerIndex->hasMapping()) {
+        $mappings = $this->customerIndex->getMapping();
+        $this->assertTrue($this->customerIndex->hasMapping());
             $this->assertArrayHasKey('mappings', $settings);
-        }
+            $this->assertSame($settings['mappings'],$mappings);
     }
 }
