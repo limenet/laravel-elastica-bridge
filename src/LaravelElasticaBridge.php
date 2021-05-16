823b9aee8167f9a0e5cfed8de183e68461992619
Linus Metzler <hi@linusmetzler.me>
2021-05-16 06:21:58 +0000

    cleanup

--- src/LaravelElasticaBridge.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Limenet\LaravelElasticaBridge;
-
-class LaravelElasticaBridge
-{
-}
--- src/LaravelElasticaBridgeFacade.php
+++ src/LaravelElasticaBridgeFacade.php
@@ -5,9 +5,6 @@ namespace Limenet\LaravelElasticaBridge;
 use Illuminate\Support\Facades\Facade;
 use Limenet\LaravelElasticaBridge\Client\ElasticaClient;
 
-/**
- * @see \Limenet\LaravelElasticaBridge\LaravelElasticaBridge
- */
 class LaravelElasticaBridgeFacade extends Facade
 {
     protected static function getFacadeAccessor()
