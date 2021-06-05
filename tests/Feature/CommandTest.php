5abf180b2b082e9c27a0f6f1d8803d496cb965b6
Linus Metzler <hi@linusmetzler.me>
2021-06-05 12:35:16 +0000

    test shouldIndex()

--- tests/Feature/CommandTest.php
+++ tests/Feature/CommandTest.php
@@ -2,6 +2,9 @@
 
 namespace Limenet\LaravelElasticaBridge\Tests\Feature;
 
+use Elastica\Query\MatchQuery;
+use Limenet\LaravelElasticaBridge\Index\IndexInterface;
+
 class CommandTest extends TestCase
 {
     /** @test */
@@ -24,5 +27,13 @@ class CommandTest extends TestCase
         $activeOld = $this->productIndex->getBlueGreenActiveElasticaIndex()->getName();
         $this->index($this->productIndex);
         $this->assertNotSame($activeOld, $this->productIndex->getBlueGreenActiveElasticaIndex()->getName());
+    }
+    /** @test */
+    public function index_command_respects_should_index()
+    {
+        $this->index($this->customerIndex);
+
+        $this->assertCount(0, $this->customerIndex->searchForElements(new MatchQuery(IndexInterface::DOCUMENT_MODEL_ID, 1)));
+        $this->assertCount(1, $this->customerIndex->searchForElements(new MatchQuery(IndexInterface::DOCUMENT_MODEL_ID, 2)));
     }
 }
