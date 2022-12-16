723523ec5d03addba231b7d76b21c7e3531c5ceb
Linus Metzler <hi@linusmetzler.me>
2022-12-16 10:51:52 +0100

    add delete command (#8)

---
+++ src/Commands/DeleteCommand.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Limenet\LaravelElasticaBridge\Commands;
+
+use Illuminate\Console\Command;
+use Limenet\LaravelElasticaBridge\Client\ElasticaClient;
+use Limenet\LaravelElasticaBridge\Repository\IndexRepository;
+
+class DeleteCommand extends Command
+{
+    protected $signature = 'elastica-bridge:delete {--force}';
+
+    protected $description = 'Delete all Elasticsearch indices known to the package';
+
+    public function __construct(
+        protected ElasticaClient $elastica,
+        protected IndexRepository $indexRepository
+    ) {
+        parent::__construct();
+    }
+
+    public function handle(): int
+    {
+        if (! $this->option('force') && ! $this->confirm('Do you want to proceed? (y/N)', false)) {
+            return self::SUCCESS;
+        }
+
+        foreach ($this->inidices() as $index) {
+            $client = $this->elastica->getIndex($index);
+            if (! $client->exists()) {
+                continue;
+            }
+            $this->info($index);
+
+            foreach ($client->getAliases() as $alias) {
+                $this->info($alias);
+                $client->removeAlias($alias);
+            }
+
+            $client->delete();
+        }
+
+        return self::SUCCESS;
+    }
+
+    private function inidices(): array
+    {
+        $indices = [];
+
+        foreach ($this->indexRepository->all() as $indexConfig) {
+            if ($indexConfig->hasBlueGreenIndices()) {
+                $indices[] = $indexConfig->getBlueGreenActiveElasticaIndex()->getName();
+                $indices[] = $indexConfig->getBlueGreenInactiveElasticaIndex()->getName();
+
+                continue;
+            }
+
+            $indices[] = $indexConfig->getName();
+        }
+
+        return $indices;
+    }
+}
