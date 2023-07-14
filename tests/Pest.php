b21dd5ff9fb14bb037b25bc2330b5db349fc5890
Linus Metzler <hi@linusmetzler.me>
2023-07-14 08:48:34 +0200

    cleanup

--- tests/Pest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Illuminate\Foundation\Testing\RefreshDatabaseState;
-use Limenet\LaravelElasticaBridge\Tests\TestCase;
-
-uses(TestCase::class)->in(__DIR__);
-uses(RefreshDatabaseState::class);
