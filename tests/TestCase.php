e5358046f3abd681d2166409312ee737495d691a
Linus Metzler <hi@linusmetzler.me>
2024-03-17 12:22:26 +0100

    Laravel 11 (#25)
    * update dev dependencies
    
    Fix styling
    
    Fix styling
    
    * Laravel 11

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Limenet\LaravelElasticaBridge\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Database\Schema\Blueprint;
 use Limenet\LaravelElasticaBridge\LaravelElasticaBridgeServiceProvider;
 use Limenet\LaravelElasticaBridge\Tests\App\Elasticsearch\CustomerIndex;
 use Limenet\LaravelElasticaBridge\Tests\App\Elasticsearch\InvoiceIndex;
@@ -16,11 +16,11 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    use RefreshDatabase;
-
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->setUpDatabase();
 
         Factory::guessFactoryNamesUsing(
             fn (string $modelName) => 'Limenet\\LaravelElasticaBridge\\Tests\\Database\\Factories\\'.class_basename($modelName).'Factory'
@@ -51,10 +51,67 @@ class TestCase extends Orchestra
         ];
     }
 
-    protected function defineEnvironment($app): void
+    protected function setUpDatabase()
     {
-        $migration = include __DIR__.'/database/migrations/SetupTables.php';
+        $schema = $this->app['db']->connection()->getSchemaBuilder();
 
-        $migration->up();
+        $schema->create('customers', function (Blueprint $table): void {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('type');
+            $table->timestamps();
+        });
+
+        $schema->create('invoices', function (Blueprint $table): void {
+            $table->uuid()->primary();
+            $table->string('status');
+            $table->timestamps();
+        });
+
+        $schema->create('products', function (Blueprint $table): void {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        $schema->create('orders', function (Blueprint $table): void {
+            $table->increments('id');
+            $table->dateTime('ordered_at');
+            $table->timestamps();
+        });
+
+        $schema->create('failed_jobs', function (Blueprint $table): void {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->text('connection');
+            $table->text('queue');
+            $table->longText('payload');
+            $table->longText('exception');
+            $table->timestamp('failed_at')->useCurrent();
+        });
+
+        $schema->create('jobs', function (Blueprint $table): void {
+            $table->bigIncrements('id');
+            $table->string('queue')->index();
+            $table->longText('payload');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+        });
+
+        $schema->create('job_batches', function (Blueprint $table): void {
+            $table->string('id')->primary();
+            $table->string('name');
+            $table->integer('total_jobs');
+            $table->integer('pending_jobs');
+            $table->integer('failed_jobs');
+            $table->text('failed_job_ids');
+            $table->text('options')->nullable();
+            $table->integer('cancelled_at')->nullable();
+            $table->integer('created_at');
+            $table->integer('finished_at')->nullable();
+        });
     }
 }
