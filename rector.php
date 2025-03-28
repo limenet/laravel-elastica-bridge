ebf20927a3752f8d04ba877efbf4c2f30d4dc2e5
Linus Metzler <hi@linusmetzler.me>
2025-03-28 19:53:38 +0100

    updates

--- rector.php
+++ rector.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
-use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertCountWithZeroToAssertEmptyRector;
 use RectorLaravel\Set\LaravelSetList;
 
 return RectorConfig::configure()
@@ -39,5 +38,4 @@ return RectorConfig::configure()
         StringClassNameToClassConstantRector::class => [
             'src/Client/ElasticaClient.php',
         ],
-        AssertCountWithZeroToAssertEmptyRector::class,
     ]);
